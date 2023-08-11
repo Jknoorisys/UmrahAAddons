f18cf6bdbe9a050981b8ab82d59bf54525446c96
Javeriya <javeriya.noorisys@gmail.com>
2023-08-11 15:38:07 +0530

    bug

--- app/Controllers/ManageDuas.php
+++ app/Controllers/ManageDuas.php
@@ -146,14 +146,14 @@ class ManageDuas extends BaseController
             $db = db_connect();
             if ($language == 'en') {
                 $duasData = $db->table('tbl_duas as s')
-                                ->select('s.id, s.user_id, s.user_type, s.title_en as title, s.reference_en as reference, s.image, s.type, s.created_at, s.updated_at')
+                                ->select('s.id, s.user_id, s.user_type, s.title_en as title, s.reference_en as reference, s.image, s.type, s.status, s.created_at, s.updated_at')
                                 ->where($whereCondition)
                                 ->orderBy('s.id', 'DESC')
                                 ->limit($limit, $offset)
                                 ->get()->getResult();
             } else {
                 $duasData = $db->table('tbl_duas as s')
-                                ->select('s.id, s.user_id, s.user_type, s.title_ur as title, s.reference_ur as reference, s.image, s.type, s.created_at, s.updated_at')
+                                ->select('s.id, s.user_id, s.user_type, s.title_ur as title, s.reference_ur as reference, s.image, s.type, s.status, s.created_at, s.updated_at')
                                 ->where($whereCondition)
                                 ->orderBy('s.id', 'DESC')
                                 ->limit($limit, $offset)
